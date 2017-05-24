c926480188cb93e220127fa53c7a27d2ee6c7959
wavis421 <wendy.avis@jointheleague.org>
2017-05-24 11:09:27 -0700

    Fixed Foreign Key Constraints in CreateTables procedure.

--- CreateTables.sql
+++ CreateTables.sql
@@ -8,31 +8,33 @@ BEGIN
 		ProgramID int(11) NOT NULL AUTO_INCREMENT,
 		PRIMARY KEY (ProgramID),
 		
+		# Program data
 		ProgramName varchar(60) DEFAULT NULL,
 		StartDate date DEFAULT NULL,
 		EndDate date DEFAULT NULL
-	);
+	) ENGINE=InnoDB;
 	
 	CREATE TABLE IF NOT EXISTS Persons (
 		PersonID int(11) NOT NULL AUTO_INCREMENT,
 		PRIMARY KEY (PersonID),
 		
+		# Person data
 		PersonName varchar(50) DEFAULT NULL,
 		PhoneNumber varchar(12) DEFAULT NULL,
 		EMail varchar(50) DEFAULT NULL,
 		isLeader tinyint(1) DEFAULT NULL,
 		Notes varchar(140) DEFAULT NULL
-	);
+	) ENGINE=InnoDB;
 	
 	CREATE TABLE IF NOT EXISTS Tasks (
 		TaskID int(11) NOT NULL AUTO_INCREMENT,
 		PRIMARY KEY (TaskID),
-		
 		ProgramID int(11),
-		INDEX prog_idx (ProgramID),
-		FOREIGN KEY (ProgramID) REFERENCES Programs(ProgramID) 
-			ON DELETE CASCADE
-			ON UPDATE CASCADE,
+		
+		CONSTRAINT fk_tasks_program_id
+			FOREIGN KEY (ProgramID) 
+			REFERENCES Programs(ProgramID) 
+			ON DELETE CASCADE,
 		
 		# Task data
 		TaskName varchar(30) DEFAULT NULL,
@@ -44,68 +46,71 @@ BEGIN
 		DowInMonth int(11) DEFAULT NULL,
 		Hour int(11) DEFAULT NULL,
 		Minute int(11) DEFAULT NULL
-	);
+	) ENGINE=InnoDB;
 	
 	CREATE TABLE IF NOT EXISTS SingleInstanceTasks (
 		SingleInstanceID int(11) NOT NULL AUTO_INCREMENT,
 		PRIMARY KEY (SingleInstanceID),
-		
 		ProgramID int(11),
-		INDEX prog_idx (ProgramID),
-		FOREIGN KEY (ProgramID) REFERENCES Programs(ProgramID) 
-			ON DELETE CASCADE
-			ON UPDATE CASCADE,
-		
 		TaskID int(11),
-		INDEX task_idx (TaskID),
-		FOREIGN KEY (TaskID) REFERENCES Tasks(TaskID) 
-			ON DELETE CASCADE
-			ON UPDATE CASCADE,
-		
 		PersonID int(11),
-		INDEX person_idx (PersonID),
-		FOREIGN KEY (PersonID) REFERENCES Persons(PersonID) 
-			ON DELETE CASCADE
-			ON UPDATE CASCADE,
 		
+		CONSTRAINT fk_single_program_id
+			FOREIGN KEY (ProgramID) 
+			REFERENCES Programs(ProgramID) 
+			ON DELETE CASCADE,
+		
+		CONSTRAINT fk_single_task_id
+			FOREIGN KEY (TaskID) 
+			REFERENCES Tasks(TaskID) 
+			ON DELETE CASCADE,
+		
+		CONSTRAINT fk_single_person_id
+			FOREIGN KEY (PersonID) 
+			REFERENCES Persons(PersonID) 
+			ON DELETE CASCADE,
+		
+		# Single instance task data
 		SingleDate date DEFAULT NULL,
 		SingleTime time DEFAULT NULL,
 		Color int(11) DEFAULT 0
-	);
+	) ENGINE=InnoDB;
 	
 	CREATE TABLE IF NOT EXISTS AssignedTasks (
 		AssignedTaskID int(11) NOT NULL AUTO_INCREMENT,
 		PRIMARY KEY (AssignedTaskID),
-		
 		PersonID int(11),
-		INDEX person_idx (PersonID),
-		FOREIGN KEY (PersonID) REFERENCES Persons(PersonID) 
-			ON DELETE CASCADE
-			ON UPDATE CASCADE,
-		
 		TaskID int(11),
-		INDEX task_idx (TaskID),
-		FOREIGN KEY (TaskID) REFERENCES Tasks(TaskID) 
-			ON DELETE CASCADE
-			ON UPDATE CASCADE,
 		
+		CONSTRAINT fk_assigned_person_id
+			FOREIGN KEY (PersonID) 
+			REFERENCES Persons(PersonID) 
+			ON DELETE CASCADE,
+		
+		CONSTRAINT fk_assigned_task_id
+			FOREIGN KEY (TaskID) 
+			REFERENCES Tasks(TaskID) 
+			ON DELETE CASCADE,
+		
+		# Assigned task data
 		DaysOfWeek int(11) DEFAULT NULL,
 		DowInMonth int(11) DEFAULT NULL
-	);
+	) ENGINE=InnoDB;
 	
 	CREATE TABLE IF NOT EXISTS UnavailDates (
 		UnavailDatesID int(11) NOT NULL AUTO_INCREMENT,
 		PRIMARY KEY (UnavailDatesID),
-		
 		PersonID int(11),
-		INDEX person_idx (PersonID),
-		FOREIGN KEY (PersonID) REFERENCES Persons(PersonID) 
-			ON DELETE CASCADE
-			ON UPDATE CASCADE,
 		
+		CONSTRAINT fk_unavail_person_id
+			FOREIGN KEY (PersonID) 
+			REFERENCES Persons(PersonID) 
+			ON DELETE CASCADE,
+		
+		# Unavail dates data
 		StartDate date DEFAULT NULL,
 		EndDate date DEFAULT NULL
-	);
+	) ENGINE=InnoDB;
 
 END$$
 DELIMITER ;
